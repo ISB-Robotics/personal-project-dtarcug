bee288437930fc6499d65d59a0a3a5514bbc45e5
Ryan Wong <ryanwong1110@yahoo.com>
2021-05-18 10:41:38 +0800

    .

--- get_board.py
+++ get_board.py
@@ -3,81 +3,71 @@ import numpy as np
 from time import sleep
 from collections import Counter
 
-image = cv2.imread('example.jpg')
-size = 100
-small = cv2.resize(image, (size, size))
-rgb = cv2.cvtColor(small, cv2.COLOR_BGR2RGB)
+def get(file):
+    """gets the board state given filename or image"""
 
-lower_range = np.array([150,0, 0])
-upper_range = np.array([255, 120, 120])
+    #load image and resize it so it is easier to work with
+    image = cv2.imread(file) if type(file) == str else file
+    size = 100
+    small = cv2.resize(image, (size, size))
+    rgb = cv2.cvtColor(small, cv2.COLOR_BGR2RGB)
 
-mask = cv2.inRange(rgb, lower_range, upper_range)
+    #mask for the boarder
+    lower_range = np.array([150,0, 0])
+    upper_range = np.array([255, 120, 120])
+    mask = cv2.inRange(rgb, lower_range, upper_range)
 
-top, bottom, left, right = [None] * 4
-cap = size * 0.4
+    #find the rectangle of board
+    boundaries = [None] * 4
+    cap = size * 0.4
+    for index, side in enumerate([mask, mask.transpose(), mask[::-1], mask.transpose()[::-1]]):
+        for index2, row in enumerate(side):
+            if Counter(row)[255] >= cap:
+                boundaries[index] = size - index2 if index >= 2 else index2
+            elif boundaries[index] is not None:
+                break
+        else:
+            return None
+    top, left, bottom, right = boundaries
 
-for index, row in enumerate(mask):
-    if Counter(row)[255] >= cap:
-        top = index
-    elif top is not None:
-        break
+    if abs(top - bottom) < 10 or abs(left - right) < 10:
+        return None
 
-for index, col in enumerate(mask.transpose()):
-    if Counter(col)[255] >= cap:
-        left = index
-    elif left is not None:
-        break
+    #mask for white and black pieces
+    b_lower_range = np.array([0]*3)
+    b_upper_range = np.array([60]*3)
+    black_piece_mask = cv2.inRange(rgb, b_lower_range, b_upper_range)
+    w_lower_range = np.array([180]*3)
+    w_upper_range = np.array([[255] * 3])
+    white_piece_mask = cv2.inRange(rgb, w_lower_range, w_upper_range)
 
-for index, row in enumerate(mask[::-1]):
-    if Counter(row)[255] >= cap:
-        bottom = size - index - 1
-    elif bottom is not None:
-        break
+    #detect the piece in each cell
+    board = np.zeros((8, 8))
+    v_range = (bottom - top)/8
+    h_range = (right - left)/8
+    cap = v_range * h_range * 0.2
+    for row in range(8):
+        for col in range(8):
+            if (row + col) % 2 == 0:
+                continue
+            square = black_piece_mask[int(top+v_range*row):int(top+v_range*(row+1)),
+                     int(left+h_range*col):int(left+h_range*(col+1))].flatten()
+            if Counter(square)[255] > cap:
+                board[row, col] = -1
+            square = white_piece_mask[int(top + v_range * row):int(top + v_range * (row + 1)),
+                     int(left + h_range * col):int(left + h_range * (col + 1))].flatten()
+            if Counter(square)[255] > cap:
+                board[row, col] = 1
+    return board.tolist()
 
-for index, col in enumerate(mask.transpose()[::-1]):
-    if Counter(col)[255] >= cap:
-        right = size - index - 1
-    elif right is not None:
-        break
-small = cv2.rectangle(small, (left, top), (right, bottom), (255, 0, 0))
+def board_to_text(board):
+    """returns a string that represents the board given"""
 
-b_lower_range = np.array([0, 0, 0])
-b_upper_range = np.array([50, 50, 50])
-
-black_piece_mask = cv2.inRange(rgb, b_lower_range, b_upper_range)
-
-w_lower_range = np.array([180, 180, 230])
-w_upper_range = np.array([[255] * 3])
-white_piece_mask = cv2.inRange(rgb, w_lower_range, w_upper_range)
-
-board = np.array([[0] * 8] * 8)
-v_range = (bottom - top)/8
-h_range = (right - left)/8
-cap = v_range * h_range * 0.3
-for row in range(8):
-    for col in range(8):
-        square = black_piece_mask[int(top+v_range*row):int(top+v_range*(row+1)),
-                 int(left+h_range*col):int(left+h_range*(col+1))].flatten()
-        if Counter(square)[255] > cap:
-            board[row, col] = -1
-        square = white_piece_mask[int(top + v_range * row):int(top + v_range * (row + 1)),
-                 int(left + h_range * col):int(left + h_range * (col + 1))].flatten()
-        if Counter(square)[255] > cap:
-            board[row, col] = 1
-
-text = '+---' * 8 + '+\n'
-for row in board:
-    text += '|'
-    for col in row:
-        text += f' {"O" if col == 1 else "X" if col == -1 else " "} |'
-    text += '\n' + '+---' * 8 + '+\n'
-text = text[:-1]
-
-print(text)
-
-# cv2.imshow('border', black_piece_mask)
-# cv2.imshow('image', small)
-#
-# c = cv2.waitKey(0)
-# if c == 'q':
-#     cv2.destroyAllWindows()
+    text = '+---' * 8 + '+\n'
+    for row in board:
+        text += '|'
+        for col in row:
+            text += f' {"O" if col == 1 else "X" if col == -1 else " "} |'
+        text += '\n' + '+---' * 8 + '+\n'
+    text = text[:-1]
+    return text
---
+++ main.py
@@ -0,0 +1,29 @@
+from get_board import *
+
+"""prints board every time the board state changes"""
+
+vid = cv2.VideoCapture(0)
+
+board = None
+past_boards = []
+
+while True:
+    _, image = vid.read()
+
+    #change board if the board is consistent for 5 consecutive frames
+    temp_board = get(image)
+    if (temp_board is not None) and temp_board != board:
+        past_boards.append(temp_board)
+        if len(past_boards) > 5:
+            past_boards.pop(0)
+        if past_boards.count(temp_board) == 5:
+            board = temp_board
+            cv2.imshow('board', cv2.resize(image, (100, 100)))
+            print(board_to_text(board))
+
+    cv2.imshow("window", image)
+    c = cv2.waitKey(1)
+    if c == 'q':
+        break
+cv2.destroyAllWindows()
+
