3349bc433258335211f321411f434e0c89a5a446
dtarcug <65053378+dtarcug@users.noreply.github.com>
2021-05-26 14:21:49 +0800

    .

--- inverse_kinematics.py
+++ inverse_kinematics.py
@@ -1,9 +1,27 @@
 import math
 
+def equation(a, b, c, j):
+    return b + a * math.sqrt(1-(c * math.sin(j) / a)**2) + c * math.cos(j)
+
 def get_angles(position, lengths, cell_size, border):
     y, x = position
-    angles = [0] * (len(lengths) + 1)
-    angles[0] = math.atan2((y-3.5)*cell_size, border+(x+0.5)*cell_size)
-    angles = [math.degrees(i) for i in angles]
-    return angles
-print(get_angles((7, 7), [], 27.5, 15))
+    a, b, c = lengths
+    bottom = math.atan2((y-3.5)*cell_size, border+(x+0.5)*cell_size)
+
+    distance = math.sqrt(((y-3.5)*cell_size)**2 + (border+(x+0.5)*cell_size)**2)
+    j = 0
+    while abs(equation(a, b, c, j) - distance) > 1:
+        j += 0.01
+    k = math.asin(c * math.sin(j) / a)
+    bottom, j, k = [(math.degrees(i)+180) % 360 - 180 for i in [bottom, j, k]]
+
+    return [bottom, j, 180 - j, 180 - k]
+a, b, c = 168, 95, 109
+border = 15
+cell_size = 27.5
+y, x = 4, 7
+angles = get_angles((y, x), [a, b, c], cell_size, border)
+print(angles)
+
+print(math.cos(math.radians(angles[1])) * c + b + math.cos(math.radians(180-angles[-1])) * a)
+print(math.sqrt(((y-3.5)*cell_size)**2 + (border+(x+0.5)*cell_size)**2))
