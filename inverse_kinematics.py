b3d36765b9aa1bcc8d846db5586eb321ea2db237
dtarcug <65053378+dtarcug@users.noreply.github.com>
2021-05-28 09:58:18 +0800

    .

--- inverse_kinematics.py
+++ inverse_kinematics.py
@@ -10,18 +10,9 @@ def get_angles(position, lengths, cell_size, border):
 
     distance = math.sqrt(((y-3.5)*cell_size)**2 + (border+(x+0.5)*cell_size)**2)
     j = 0
-    while abs(equation(a, b, c, j) - distance) > 1:
+    while abs(equation(a, b, c, j) - distance) > 1 and j < 180:
         j += 0.01
     k = math.asin(c * math.sin(j) / a)
     bottom, j, k = [(math.degrees(i)+180) % 360 - 180 for i in [bottom, j, k]]
 
     return [bottom, j, 180 - j, 180 - k]
-a, b, c = 168, 95, 109
-border = 15
-cell_size = 27.5
-y, x = 4, 7
-angles = get_angles((y, x), [a, b, c], cell_size, border)
-print(angles)
-
-print(math.cos(math.radians(angles[1])) * c + b + math.cos(math.radians(180-angles[-1])) * a)
-print(math.sqrt(((y-3.5)*cell_size)**2 + (border+(x+0.5)*cell_size)**2))
